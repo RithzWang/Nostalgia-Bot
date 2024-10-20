be6e809596bd40b122cc0f55175bdbd31241fa56
Rithz <101114513+RithzWang@users.noreply.github.com>
2024-10-20 11:44:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Discord = require('discord.js');
 Discord.Constants.DefaultOptions.ws.properties.$browser = "Discord Android";
 const client = new Discord.Client();
 const axios = require('axios');
+const langDetector = require('lang-detector');
+const googleTranslate = require('google-translate');
 
 const keep_alive = require('./keep_alive.js')
 
@@ -84,6 +86,24 @@ client.on('message', message => {
   
 });
 
+
+
+// -------- auto translation -------- //
+
+
+client.on('message', (message) => {
+  const detectedLanguage = langDetector.detect(message.content);
+
+  if (detectedLanguage !== 'en') {
+    const targetLanguage = 'en';
+    googleTranslate.translate(message.content, targetLanguage, (err, translation) => {
+      message.channel.send(`**Translation:** ${translation}`);
+    });
+  }
+});
+
+
+// ----------------------------------- //
 
 
 // --------- welcomer --------- //

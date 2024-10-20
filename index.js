e9d6b0262b944724a22410a1c0fef025d406966a
Rithz <101114513+RithzWang@users.noreply.github.com>
2024-10-21 01:40:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
+    if (command.aliases) {
+        for (const alias of command.aliases) {
+            client.commands.set(alias, command);
+        }
+    }
 }
 // ----------------------------  //
 

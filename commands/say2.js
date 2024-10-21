7becb1d922c42fddd9fe76c652952c53c030e5de
Rithz <101114513+RithzWang@users.noreply.github.com>
2024-10-21 07:20:40 +0700

    Update say2.js

--- commands/say2.js
+++ commands/say2.js
@@ -1,8 +1,10 @@
+const { PermissionsBitField } = require('discord.js');
+
 module.exports = {
     name: 'say2',
     execute(message, args) {
         // Check if the user has administrator permissions
-        if (message.member.permissions.has('ADMINISTRATOR')) {
+        if (message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
             // Check if the first argument is a channel mention
             const channelMention = args[0];
             const targetChannel = message.mentions.channels.first() || message.guild.channels.cache.get(channelMention.replace(/<#(\d+)>/, '$1'));
@@ -29,14 +31,23 @@ module.exports = {
             targetChannel.send(content, silentMessageOptions)
                 .then(() => {
                     // Optionally, send a confirmation message in the original channel
-                    message.channel.send('Message sent successfully to ' + targetChannel.toString() + '!');
+                    message.channel.send('Message sent successfully to ' + targetChannel.toString() + '!')
+                        .catch(err => {
+                            console.error('Failed to send confirmation message:', err);
+                        });
                 })
                 .catch(err => {
                     console.error('Failed to send message:', err);
-                    message.channel.send('Failed to send message to the specified channel.');
+                    message.channel.send('Failed to send message to the specified channel.')
+                        .catch(err => {
+                            console.error('Failed to send error message:', err);
+                        });
                 });
         } else {
-            message.channel.send('You do not have permission to use this command.');
+            message.channel.send('You do not have permission to use this command.')
+                .catch(err => {
+                    console.error('Failed to send permission denied message:', err);
+                });
         }
     }
 };

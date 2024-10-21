201c37e8389797253d5d2f41bf446912e34478ed
Rithz <101114513+RithzWang@users.noreply.github.com>
2024-10-21 07:40:42 +0700

    Update edit.js

--- commands/edit.js
+++ commands/edit.js
@@ -1,24 +1,17 @@
-const { PermissionsBitField } = require('discord.js');
-
 module.exports = {
-    name: 'edit',
+        name: 'edit',
     execute(message, args) {
-        // Check if the user has administrator permissions
-        if (message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-            // Delete the command message
-            message.delete();
-
+        if (message.member.permissions.has('ADMINISTRATOR')) {
+            message.delete()
             const messageId = args[0];
             const newContent = args.slice(1).join(' ');
 
             const silentMessageOptions = {
-    allowedMentions: {
-        parse: ['users', 'roles', 'everyone'].map(x => ({ [x]: false })),
-        repliedUser: false,
-    },
-};
+                allowedMentions: {
+                    parse: [], // Don't parse any mentions
+                },
+            };
 
-            // Fetch the message to edit
             message.channel.messages.fetch(messageId)
                 .then(msg => {
                     msg.edit(newContent, silentMessageOptions)
@@ -34,3 +27,4 @@ module.exports = {
         }
     }
 };
+                          

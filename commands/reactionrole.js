772913a2bbb6c4bc2f3673ebfba49a1bc311140a
Rithz <101114513+RithzWang@users.noreply.github.com>
2024-10-21 15:38:46 +0700

    Update reactionrole.js

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -1,3 +1,6 @@
+const { MessageEmbed } = require('discord.js'); // Import MessageEmbed
+const enabledReactionRoles = new Map(); // To keep track of channels with enabled reaction roles
+
 module.exports = {
     name: 'reactionrole',
     aliases: ['rr'],
@@ -8,34 +11,47 @@ module.exports = {
         }
 
         // Check if the correct number of arguments is provided
-        if (args.length < 3) {
-            return message.channel.send('Usage: !reactionrole [message id] [role name] [emoji]');
+        if (args.length < 4) {
+            return message.channel.send('Usage: !reactionrole [channel mention] [message ID] [role mention/id] [emoji]');
         }
 
-        const messageId = args[0]; // The ID of the message to react to
-        const roleName = args[1]; // The name of the role to assign
-        const emoji = args[2]; // The emoji to react with
+        const channelMention = args[0]; // The channel mention
+        const messageId = args[1]; // The message ID as the 2nd argument
+        const roleArg = args[2]; // The role mention or ID
+        const emoji = args[3]; // The emoji to react with
 
-        // Find the role by name
-        const role = message.guild.roles.cache.find(r => r.name === roleName);
-        if (!role) {
-            return message.channel.send('Role not found.');
+        // Fetch the target channel
+        const targetChannel = message.mentions.channels.first() || message.guild.channels.cache.get(channelMention.replace(/[<#>]/g, ''));
+        if (!targetChannel) {
+            return message.channel.send('Channel not found. Please mention a valid channel.');
         }
 
-        // Fetch the message by ID
+        // Fetch the message by ID in the specified channel
         let targetMessage;
         try {
-            targetMessage = await message.channel.messages.fetch(messageId);
+            targetMessage = await targetChannel.messages.fetch(messageId);
         } catch (error) {
             return message.channel.send('Message not found. Please check the message ID.');
+        }
+
+        // Find the role by mention or ID
+        const role = message.mentions.roles.first() || message.guild.roles.cache.get(roleArg);
+        if (!role) {
+            return message.channel.send('Role not found.');
         }
 
         // Add the reaction to the specified message
         await targetMessage.react(emoji);
 
+        // Store the reaction role in a map for this channel
+        if (!enabledReactionRoles.has(targetChannel.id)) {
+            enabledReactionRoles.set(targetChannel.id, []);
+        }
+        enabledReactionRoles.get(targetChannel.id).push({ role, emoji });
+
         // Create a reaction collector
         const filter = (reaction, user) => {
-            return reaction.emoji.name === emoji && !user.bot; // Only collect the specified emoji from non-bots
+            return reaction.emoji.name === emoji && !user.bot && enabledReactionRoles.has(targetChannel.id); // Only collect if enabled
         };
 
         const collector = targetMessage.createReactionCollector(filter, { dispose: true });
@@ -44,7 +60,6 @@ module.exports = {
             const member = message.guild.members.cache.get(user.id);
             if (member) {
                 member.roles.add(role).catch(err => console.error(err));
-                message.channel.send(`${user.username} has been given the ${roleName} role!`);
             }
         });
 
@@ -52,8 +67,58 @@ module.exports = {
             const member = message.guild.members.cache.get(user.id);
             if (member) {
                 member.roles.remove(role).catch(err => console.error(err));
-                message.channel.send(`${user.username} has had the ${roleName} role removed!`);
             }
         });
+    }
+};
+
+// Command to list all reaction roles
+module.exports.listReactionRoles = {
+    name: 'reactionrole-list',
+    aliases: ['rr-list'],
+    async execute(message) {
+        const roles = enabledReactionRoles.get(message.channel.id);
+        if (!roles || roles.length === 0) {
+            return message.channel.send('No reaction roles are currently set up for this channel.');
+        }
+
+        // Create an embed message
+        const embed = new MessageEmbed()
+            .setTitle('Active Reaction Roles')
+            .setColor('#888888') // Set a color for the embed
+            .setDescription(roles.map((item, index) => `${index + 1} ${item.emoji} ${item.role}`).join('\n'));
+
+        message.channel.send(embed); // Send the embed
+    }
+};
+
+// Command to disable a reaction role by index
+module.exports.disableReactionRole = {
+    name: 'disable-reactionrole',
+    aliases: ['disable-rr'],
+    async execute(message, args) {
+        // Check if the user has the required permissions
+        if (!message.member.permissions.has('ADMINISTRATOR')) {
+            return message.channel.send('You do not have permission to use this command.');
+        }
+
+        // Check if an index is provided
+        if (args.length < 1) {
+            return message.channel.send('Usage: !disable-reactionrole [index]');
+        }
+
+        const index = parseInt(args[0]) - 1; // Convert to zero-based index
+        const roles = enabledReactionRoles.get(message.channel.id);
+        if (!roles || index < 0 || index >= roles.length) {
+            return message.channel.send('Invalid index. Please provide a valid number from the list.');
+        }
+
+        // Remove the specified role from the list
+        roles.splice(index, 1);
+        if (roles.length === 0) {
+            enabledReactionRoles.delete(message.channel.id);
+        }
+
+        message.channel.send(`Reaction role ${index + 1} has been disabled.`);
     }
 };

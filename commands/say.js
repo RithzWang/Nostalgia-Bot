bf5fd2166090a46365430807c70023c5b12608c1
Rithz <101114513+RithzWang@users.noreply.github.com>
2024-10-21 07:41:49 +0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,32 +1,17 @@
-const { PermissionsBitField } = require('discord.js');
-
 module.exports = {
     name: 'say',
     execute(message, args) {
-        // Check if the user has administrator permissions
-        if (message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+        if (message.member.permissions.has('ADMINISTRATOR')) {
             const content = args.join(' ');
             const silentMessageOptions = {
-    allowedMentions: {
-        parse: ['users', 'roles', 'everyone'].map(x => ({ [x]: false })),
-        repliedUser: false,
-    },
-};
-
-            // Delete the command message
+                allowedMentions: {
+                    parse: [], // Don't parse any mentions
+                },
+            };
             message.delete();
-
-            // Send the message with the specified content
-            message.channel.send(content, silentMessageOptions)
-                .catch(error => {
-                    console.error('Error sending message:', error);
-                });
+            message.channel.send(content, silentMessageOptions);
         } else {
-            // If the user does not have permission, send a message
-            message.channel.send('You do not have permission to use this command.')
-                .catch(error => {
-                    console.error('Error sending permission denied message:', error);
-                });
+            message.channel.send('You do not have permission to use this command.');
         }
     }
 };

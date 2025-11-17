4c95ce48886d9bdf5df097653458871ae3c7a297
RithzWang <101114513+RithzWang@users.noreply.github.com>
2025-11-17 18:55:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ client.on('guildMemberAdd', async (member) => {
     const embed = new Discord.MessageEmbed()
       .setTitle('👋 Welcome to A2-Q Server')
       .setDescription(
-  `-# <@${memberId}> [\`${memberName}\`]\n-#Account Created: ${accountCreated}\n-#Member Count: \`${memberCount}\`\n-#Invited by <@${inviterId}> ([\`${inviteCode}\`](https://discord.gg/${inviteCode})`
+  `-# <@${memberId}> [\`${memberName}\`]\n-#Account Created: ${accountCreated}\n-#Member Count: \`${memberCount}\`\n-#Invited by <@${inviterId}> (\`${inviteCode}\`)`
 )
       .setColor(colourEmbed)
       .setFooter(`• ${member.user.username}`, member.user.displayAvatarURL())

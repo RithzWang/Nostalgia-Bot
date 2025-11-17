98af648ed3885f6b02e9cc7f28de7e3c2488f75b
RithzWang <101114513+RithzWang@users.noreply.github.com>
2025-11-18 05:01:52 +0700

    Update deleteCommands.js

--- deleteCommands.js
+++ deleteCommands.js
@@ -8,14 +8,12 @@ const rest = new REST().setToken(token);
 
 // ...
 
-// for guild-based commands
 rest
-	.delete(Routes.applicationGuildCommand(clientId, guildId, 'commandId'))
-	.then(() => console.log('Successfully deleted guild command'))
+	.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+	.then(() => console.log('Successfully deleted all guild commands.'))
 	.catch(console.error);
-
 // for global commands
 rest
-	.delete(Routes.applicationCommand(clientId, 'commandId'))
-	.then(() => console.log('Successfully deleted application command'))
+	.put(Routes.applicationCommands(clientId), { body: [] })
+	.then(() => console.log('Successfully deleted all application commands.'))
 	.catch(console.error);

a0763b9e0797773549092fbe775788d8b35dec74
RithzWang <101114513+RithzWang@users.noreply.github.com>
2025-11-18 21:15:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { Client, GatewayIntentBits, Partials, Collection, EmbedBuilder, ActivityT
 // ... other imports
 const keep_alive = require('./keep_alive.js');
 const moment = require('moment-timezone');
+const { createCanvas, loadImage } = require('canvas');
+const { /* other imports */, AttachmentBuilder } = require('discord.js');
+
 
 const { prefix, serverID, boosterLog, welcomeLog, roleupdateLog, roleupdateMessage, roleforLog, colourEmbed, BSVerifyRole, BSVerifyRoleupdateLog, BSVerifyRoleUpdateMessage, boosterRoleId, boosterChannelId, SuggestionChannelId, staffRole } = require("./config.json");
 const config = require('./config.json');

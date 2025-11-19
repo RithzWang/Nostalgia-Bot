7685c0405f006e79f3c83d8b1791514512927ca0
RithzWang <101114513+RithzWang@users.noreply.github.com>
2025-11-19 17:58:52 +0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -8,61 +8,6 @@ const CLIENT_ID = '1167109778175168554';
 const GUILD_ID = '1167046828043276379'; 
 // ---------------------------------
 
-// --- 1. /createembed Command Definition ---
-const embedCommand = new SlashCommandBuilder()
-    .setName('createembed')
-    .setDescription('Creates a rich, custom message embed.')
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator) 
-    .addStringOption(option =>
-        option.setName('title')
-            .setDescription('The main title of the embed.')
-            .setRequired(true))
-    .addStringOption(option =>
-        option.setName('description')
-            .setDescription('The main content/description of the embed.')
-            .setRequired(true))
-    .addStringOption(option =>
-        option.setName('color')
-            .setDescription('The hex color code (e.g., #FF0000).')
-            .setRequired(false))
-    .addChannelOption(option =>
-        option.setName('channel')
-            .setDescription('The channel where the embed should be sent (defaults to current channel).')
-            .setRequired(false))
-    .addStringOption(option =>
-        option.setName('footer')
-            .setDescription('The text for the small footer at the bottom.')
-            .setRequired(false))
-    .addStringOption(option =>
-        option.setName('image')
-            .setDescription('A direct URL for the main image (must be a valid image link).')
-            .setRequired(false))
-    .addStringOption(option =>
-        option.setName('thumbnail')
-            .setDescription('A direct URL for the thumbnail image (small image in the corner).')
-            .setRequired(false));
-
-// --- 2. /edit-embed Command Definition ---
-const editEmbedCommand = new SlashCommandBuilder()
-    .setName('edit-embed')
-    .setDescription('Edits the content, color, or images of an existing embed.')
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-    .addChannelOption(option =>
-        option.setName('channel')
-            .setDescription('The channel where the message is located.')
-            .setRequired(true)
-            .addChannelTypes(ChannelType.GuildText))
-    .addStringOption(option =>
-        option.setName('message_id')
-            .setDescription('The ID of the bot message containing the embed.')
-            .setRequired(true))
-    .addStringOption(option => option.setName('title').setDescription('The new main title of the embed.').setRequired(false))
-    .addStringOption(option => option.setName('description').setDescription('The new main content/description of the embed.').setRequired(false))
-    .addStringOption(option => option.setName('color').setDescription('The new hex color code (e.g., #00FFFF).').setRequired(false))
-    .addStringOption(option => option.setName('footer').setDescription('The new text for the footer.').setRequired(false))
-    .addStringOption(option => option.setName('image').setDescription('A new direct URL for the main image.').setRequired(false))
-    .addStringOption(option => option.setName('thumbnail').setDescription('A new direct URL for the thumbnail image.').setRequired(false));
-
 // --- 3. Assemble and Deploy ---
 // This list overwrites all existing commands on the server.
 const commands = [embedCommand,editEmbedCommand].map(command => command.toJSON());

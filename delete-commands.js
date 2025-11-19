4a08345f6deca09f622f2d0d5a65d8f12eaaf403
RithzWang <101114513+RithzWang@users.noreply.github.com>
2025-11-19 18:07:43 +0700

    Create delete-commands.js

---
+++ delete-commands.js
@@ -0,0 +1,24 @@
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json'); // Make sure path is correct
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+// ...
+(async () => {
+	try {
+		console.log('Started deleting all commands.');
+
+		// 1. Delete all GUILD-based commands (for a specific server)
+		// Useful if you were testing in one specific server
+		await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] });
+		console.log('Successfully deleted all guild commands.');
+
+		// 2. Delete all GLOBAL commands (for all servers)
+		// Note: Global updates can take up to 1 hour to reflect in Discord, but usually faster
+		await rest.put(Routes.applicationCommands(clientId), { body: [] });
+		console.log('Successfully deleted all global application commands.');
+
+	} catch (error) {
+		console.error(error);
+	}
+})();

2ba4149feef8ee6f92619f27ee51626f75713902
RithzWang <101114513+RithzWang@users.noreply.github.com>
2025-12-04 19:51:12 +0700

    Update createinvite.js

--- commands/createinvite.js
+++ commands/createinvite.js
@@ -1,36 +1,46 @@
 module.exports = {
     name: 'createinvite',
     execute(message, args) {
-        // 1. (Optional) Check for the restricted role from your previous request
-        // If you want to block that specific role from creating invites, uncomment the lines below:
-        /*
+        // 1. Check for the restricted role (from previous request)
         const restrictedRoleId = '1446058693631148043';
         if (message.member.roles.cache.has(restrictedRoleId)) {
             return message.channel.send({ content: 'You have a restricted role and cannot use this command.' });
         }
-        */
 
-        // 2. Check if the user has Administrator permission
-        // You can change 'ADMINISTRATOR' to 'CREATE_INSTANT_INVITE' if you want moderators to use it too.
+        // 2. Check permissions
         if (!message.member.permissions.has('ADMINISTRATOR')) {
             return message.channel.send({ content: 'You do not have permission to use this command.' });
         }
 
-        // 3. Create the invite
-        // maxAge: 0 means "Never expires"
-        // maxUses: 0 means "Unlimited uses"
-        message.channel.createInvite({
-            maxAge: 0, 
-            maxUses: 0,
-            unique: true // Generates a new unique link every time
+        // 3. Determine the target channel
+        let targetChannel;
+
+        if (args[0]) {
+            // Case A: User provided an argument (e.g., !createinvite #general)
+            // Try to find the channel by mention OR by ID
+            targetChannel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0].replace(/\D/g, ''));
+
+            if (!targetChannel) {
+                return message.channel.send({ content: 'I could not find that channel. Please mention a valid channel.' });
+            }
+        } else {
+            // Case B: User provided no argument (e.g., !createinvite)
+            // Use the current channel
+            targetChannel = message.channel;
+        }
+
+        // 4. Create the invite
+        targetChannel.createInvite({
+            maxAge: 0, // Permanent
+            maxUses: 0, // Unlimited uses
+            unique: true
         })
         .then(invite => {
-            // 4. Send the link to the chat
-            message.channel.send({ content: `Here is your permanent invite link:\n${invite.url}` });
+            message.channel.send({ content: `Here is the permanent invite link for ${targetChannel.toString()}:\n${invite.url}` });
         })
         .catch(err => {
             console.error('Failed to create invite:', err);
-            message.channel.send({ content: 'I could not create an invite. Please check my permissions (I need "Create Invite" permission).' });
+            message.channel.send({ content: `I could not create an invite for ${targetChannel.toString()}. Make sure I have "Create Invite" permissions in that channel.` });
         });
     }
 };

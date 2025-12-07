33ddd7a74cb860e5969f3ad6fed01b34115a3f71
RithzWang <101114513+RithzWang@users.noreply.github.com>
2025-12-07 14:38:17 +0700

    Update welcomeCanvas.js

--- welcomeCanvas.js
+++ welcomeCanvas.js
@@ -155,7 +155,7 @@ async function createWelcomeImage(member) {
     }
 
     // USERNAME FONT
-    ctx.font = '90px "SF Pro Regular"';
+    ctx.font = '90px "SF Pro Regular", sans-serif';
     
     ctx.fillStyle = '#b9bbbe';
     ctx.fillText(usernameText, textX, currentY);

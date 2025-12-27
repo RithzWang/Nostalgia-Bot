1d4f29fad9886282572f6326fa914b511708cb80
RithzWang <101114513+RithzWang@users.noreply.github.com>
2025-12-27 21:33:05 +0700

    Create Giveaway.js

---
+++ src/models/Giveaway.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const giveawaySchema = new mongoose.Schema({
+    guildId: { type: String, required: true },
+    channelId: { type: String, required: true },
+    messageId: { type: String, required: true },
+    hostId: { type: String, required: true },
+    prize: { type: String, required: true },
+    winnersCount: { type: Number, required: true },
+    startTimestamp: { type: Number, required: true },
+    endTimestamp: { type: Number, required: true },
+    ended: { type: Boolean, default: false },
+    participants: { type: [String], default: [] } // Array of User IDs
+});
+
+module.exports = mongoose.model('Giveaway', giveawaySchema);

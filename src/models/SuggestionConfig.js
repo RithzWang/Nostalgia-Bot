7317117dda13a6ea357ea0075a5d60283dfbb7f1
RithzWang <101114513+RithzWang@users.noreply.github.com>
2025-12-28 02:02:51 +0700

    Delete src/models/SuggestionConfig.js

--- src/models/SuggestionConfig.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const suggestionConfigSchema = new mongoose.Schema({
-    guildId: { type: String, required: true, unique: true },
-    channelId: { type: String, required: true }
-});
-
-module.exports = mongoose.model('SuggestionConfig', suggestionConfigSchema);

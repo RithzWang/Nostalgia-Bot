2014c0c17c238229c2008e295fade5e6e514cdcf
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-01-05 05:20:56 +0700

    Delete src/models/ChatBot.js

--- src/models/ChatBot.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const ChatBotSchema = new mongoose.Schema({
-    GuildID: { type: String, required: true },
-    ChannelID: { type: String, required: true }
-});
-
-module.exports = mongoose.model('ChatBot', ChatBotSchema);

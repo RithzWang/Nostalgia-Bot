299d7a24340525b8e647fdce5dcb9fc2821f4677
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-01-05 19:05:01 +0700

    Update GuessGame.js

--- src/models/GuessGame.js
+++ src/models/GuessGame.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const guessGameSchema = new mongoose.Schema({
     guildId: { type: String, required: true, unique: true },
     channelId: { type: String, required: true },
-    currentWord: { type: String, default: null }, // The real word (e.g., "APPLE")
-    displayWord: { type: String, default: null }  // The hidden version (e.g., "A_P_LE")
+    currentWord: { type: String, default: null }, 
+    displayWord: { type: String, default: null } 
 });
 
 module.exports = mongoose.model('GuessGame', guessGameSchema);

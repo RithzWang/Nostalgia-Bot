ba0fa92a16c7f312db4d7375ea0f56c115c7448e
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-01-19 00:49:06 +0700

    Update EmojiChannelScema.js

--- src/models/EmojiChannelScema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const EmojiChannelSchema = new mongoose.Schema({
-    guildId: { type: String, required: true, unique: true },
-    channelId: { type: String, required: true }
-});
-
-module.exports = mongoose.model('EmojiChannel', EmojiChannelSchema);

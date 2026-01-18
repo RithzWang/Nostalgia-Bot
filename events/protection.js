7dc389eb5b9529d04b03d7909f3e08b6bcf3b3f7
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-01-18 09:12:19 +0700

    Update protection.js

--- events/protection.js
+++ events/protection.js
@@ -78,7 +78,7 @@ module.exports = {
                 // A. REPLY (Do NOT delete)
                 await message.reply({
                     content: `⚠️ Please do not try to mention everyone/here ever again.`,
-                    stickers: ['755490897143136446'],
+                    stickers: ['749055120263872532'],
                     allowedMentions: { repliedUser: true }
                 });
 

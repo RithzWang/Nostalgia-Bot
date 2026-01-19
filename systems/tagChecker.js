8776810442bca49d6689e48e16e446850d3275a2
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-01-19 11:11:57 +0700

    Update tagChecker.js

--- systems/tagChecker.js
+++ systems/tagChecker.js
@@ -16,7 +16,7 @@ module.exports = (client) => {
 function startLoop(client) {
     console.log("✅ Tag Checker Started.");
     runSweep(client);
-    setInterval(() => runSweep(client), 60 * 1000);
+    setInterval(() => runSweep(client), 1 * 1000);
 }
 
 async function runSweep(client) {

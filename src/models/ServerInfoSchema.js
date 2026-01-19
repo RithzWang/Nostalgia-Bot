9b5a24a2a29f52b8d06f15abf36885583b358c21
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-01-19 20:43:44 +0700

    Delete ServerInfoSchema.js

--- src/models/ServerInfoSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const serverInfoSchema = new mongoose.Schema({
-    guildId: { type: String, required: true },
-    channelId: { type: String, required: true },
-    messageId: { type: String, required: true }
-});
-
-module.exports = mongoose.model('ServerInfo', serverInfoSchema);

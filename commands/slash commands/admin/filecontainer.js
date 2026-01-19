1b2440d56148e56c9ef7b4013829332fa7259c3a
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-01-19 16:25:10 +0700

    Update filecontainer.js

--- commands/slash commands/admin/filecontainer.js
+++ commands/slash commands/admin/filecontainer.js
@@ -14,7 +14,6 @@ const {
 const FileContainer = require('../../../src/models/FileContainerSchema'); 
 
 module.exports = {
-    guildOnly: true,
     data: new SlashCommandBuilder()
         .setName('filecontainer')
         .setDescription('Manage File Container Messages')

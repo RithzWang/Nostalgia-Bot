66a841893651dc1f83a01bffea2eabff210c948b
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-02-01 20:06:51 +0700

    Update banner.js

--- commands/normal commands/banner.js
+++ commands/normal commands/banner.js
@@ -49,8 +49,8 @@ module.exports = {
             // 3. Builder
             const createBannerContainer = (isShowingGlobal, disableToggle = false) => {
                 const currentImage = isShowingGlobal ? globalBanner : displayBanner;
-                const titleText = isShowingGlobal ? `## Banner Picture` : `## Pre-server Banner Picture`;
-                const bodyText = isShowingGlobal ? `-# Banner for <@${targetUser.id}>` : `-# Pre-server Banner for <@${targetUser.id}>`;
+                const titleText = isShowingGlobal ? `# Banner Picture` : `# Pre-server Banner Picture`;
+                const bodyText = isShowingGlobal ? `Banner for <@${targetUser.id}>` : `Pre-server Banner for <@${targetUser.id}>`;
 
                 const toggleButton = new ButtonBuilder()
                     .setCustomId('toggle_bn_msg')

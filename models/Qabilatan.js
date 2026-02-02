8bde6d821d5ca7d25ae00a9dca341dc2830e3e9b
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-02-03 01:12:24 +0700

    Create models/Qabilatan.js

---
+++ models/Qabilatan.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const panelSchema = new mongoose.Schema({
+    guildId: String,
+    channelId: String,
+    messageId: String
+});
+
+const serverSchema = new mongoose.Schema({
+    serverId: String, // The ID of the listed server
+    name: String,
+    inviteLink: String,
+    tagText: String, // Optional
+    tagRoleID: String, // Role in Main Server
+    addedAt: { type: Date, default: Date.now }
+});
+
+const greetSchema = new mongoose.Schema({
+    guildId: String, // The server where greeting happens
+    channelId: String
+});
+
+module.exports = {
+    Panel: mongoose.model('QabilatanPanel', panelSchema),
+    ServerList: mongoose.model('QabilatanList', serverSchema),
+    GreetConfig: mongoose.model('QabilatanGreet', greetSchema)
+};

63075c2634cb1d7e4a4f52e8cff9975897cbe936
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-06-08 04:28:18 +0700

    Update networkInteraction.js

--- events/networkInteraction.js
+++ events/networkInteraction.js
@@ -9,7 +9,7 @@ const {
 } = require('discord.js');
 
 // ✅ Updated to match your new Network branding (Make sure your model file is renamed too!)
-const { ServerList, GreetConfig } = require('../src/models/Network'); 
+const { ServerList, GreetConfig } = require('../src/models/NetworkConfig'); 
 const { updateAllPanels } = require('../utils/networkManager'); 
 
 module.exports = {

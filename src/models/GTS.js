010254694dbf4bc1506ae08823bb79abe852e95f
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-06-10 07:47:12 +0700

    Create GTS.js

---
+++ src/models/GTS.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const GTSHubSchema = new mongoose.Schema({
+    mainServerId: { type: String, required: true, unique: true },
+    dashboardChannelId: { type: String, default: null },
+    dashboardMessageId: { type: String, default: null },
+    defaultTagRole: { type: String, default: null },
+    joinMainRequired: { type: Boolean, default: false }
+});
+
+const GTSServerSchema = new mongoose.Schema({
+    serverId: { type: String, required: true, unique: true },
+    inviteLink: { type: String, default: null },
+    tagText: { type: String, default: null },
+    mainTagRole: { type: String, default: null },
+    mainLogChannel: { type: String, default: null },
+    localTagRole: { type: String, default: null },
+    localLogChannel: { type: String, default: null },
+    greetChannel: { type: String, default: null },
+    specialGuestRole: { type: String, default: null } // Ignored by Gatekeeper
+});
+
+module.exports = {
+    GTSHub: mongoose.model('GTSHub', GTSHubSchema),
+    GTSServer: mongoose.model('GTSServer', GTSServerSchema)
+};

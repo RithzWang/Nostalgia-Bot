b81f98dcd63cc474064109732e61a972d53d292a
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-06-26 04:08:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ client.on('messageCreate', async (message) => {
 
 
 // --- WELCOME LOGIC ---
-const { createWelcomeImage } = require('./welcomeCanvas9.js');
+const { createWelcomeImage } = require('./welcomeCanvas10.js');
 const { fetchAdvancedProfile } = require('./utils/v9Scraper');
 
 client.on('guildMemberAdd', async (member) => {
